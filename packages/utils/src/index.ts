fc71c440164b417ba26d72ecba0f499aa087ec0e
sulei <779647751@qq.com>
2022-09-06 00:30:02 +0800

    feat: utils update docs

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -14,5 +14,4 @@ const isNumber = (num: number): boolean =>
   Object.prototype.toString.call(num) === '[object Number]';
 
 export { sum, isString, isNumber, createRandom };
-
 export default { sum, isString, isNumber, createRandom };
