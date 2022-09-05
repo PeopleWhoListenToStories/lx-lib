ee8938aab37e46fde78a2502e5e3131410e52f7b
sulei <779647751@qq.com>
2022-09-06 00:33:03 +0800

    feat: utils update docs

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,7 +2,7 @@ const sum = (...args: number[]): number => {
   return args.reduce((pre, next) => (pre += next), 0);
 };
 
-const createRandom = (num: number = 9) =>
+const createRandom = (num: number = 10) =>
   Math.random()
     .toString()
     .slice(2, num + 2);
